ce7c430114800bd712b26af05ee193c3b1d10277
Florian Hinzpeter <florian.hinzpeter@tum.de>
2021-09-02 10:34:56 +0200

    bug fix in app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
-app.config['SQLALCHEMY_DATABASE_URI'] = os.enveron.get(uri, 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Flori'
 api = Api(app)
